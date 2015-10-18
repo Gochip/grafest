06c5361d234cd17088dc14d9633c480abcb63c0e
Gochi <germannparisi@gmail.com>
2015-10-18 18:14:15 -0300

    Agrego caja visible para mostrar el promedio

--- js/cabecera_util.js
+++ js/cabecera_util.js
@@ -1,8 +1,14 @@
 (function($){
     $.fn.calculoPromedio = function(){
-        objeto = this;
+        var objeto = this;
+        var oculto = true;
         objeto.css("cursor", "pointer");
         objeto.click(function(){
+            oculto = !oculto;
+            if(oculto) {
+                objeto.find(".spanDatos").hide();
+                return;
+            }
             var indice = objeto.index();
             var suma = 0;
             var n = 0;
@@ -11,7 +17,19 @@
                 suma += parseInt(x);
                 n++;
             });
-            console.log(suma / n);
+            var span = $("<span class='spanDatos'></span>");
+            var ancho = objeto.css("width");
+            span.css({
+                "background-color": "lightgreen",
+                "padding": "5px",
+                "position": "absolute",
+                "top": "-50px",
+                "left": ancho,
+                "border-radius": "10px"
+            });
+            var promedio = suma / n;
+            span.html("Prom: " + promedio);
+            objeto.append(span);
         });
     }
 }(jQuery));
